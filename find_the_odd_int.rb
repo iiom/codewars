59585be83d5a5313baf3ede7b4d20c44877d49b6
iiom <ioma766@gmail.com>
2018-05-21 14:05:39 +0300

    .

--- find_the_odd_int.rb
+++ find_the_odd_int.rb
@@ -13,6 +13,27 @@ end
 # end
 # find_it(array=[20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
 
+# def find_it(seq)
+#   seq.uniq.each do |val|
+#     return val if seq.count(val).odd?
+#   end
+# end
+# find_it(array=[20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
+
+# def find_it(seq)
+#   seq.each do |i|
+#     if seq.count(i) % 2 != 0
+#       return i
+#     end
+#   end
+# end
+# find_it(array=[20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
+
+# def find_it(seq)
+#   seq.uniq.each{|x| return x if seq.count(x).odd?}
+# end
+# find_it(array=[20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
+
 
 # find/ detect ==> Передаёт в указанный блок каждый элемент коллекции enum. Возвращает первый
 # элемент enum, для которого результат выполнения block не является false.
