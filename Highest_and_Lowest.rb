f97b716073f08a8cff63646ba0e1e49490afc48d
iiom <ioma766@gmail.com>
2018-05-23 20:35:06 +0300

    modified

--- Highest_and_Lowest.rb
+++ Highest_and_Lowest.rb
@@ -13,12 +13,12 @@
 #    numbers = n1
 #   p numbers.join(" ")
 # end
-# 
+#
 #   hal("-1 2 -3 4 -5 4 9 10")
 
-
+#z создание массива, конвертировать в массив цифр, найти мин макс и вывести в строку string
   def high_and_low(numbers)
-    p  numbers.split.map(&:to_i).minmax.reverse.join(' ')
+    p  numbers.split.map(&:to_i).minmax.reverse.join(' ')    #&:  способ передать блок
   end
     high_and_low("-1 2 -3 4 -5 4 9 10")
 
@@ -38,3 +38,20 @@
 #   p  numbers.split.minmax_by(&:to_i).reverse.join(' ')
 # end
 #   high_and_low3("-1 2 -3 4 -5 4 9 10")
+
+
+# split Делит строку str на подстроки по разделителю pattern
+# (который может быть как правилом, так и строкой).  str.split(pattern=$;, [limit])
+
+# map Выполняет выражение block для каждого элемента массива array.
+# Создает и возвращает новый массив, состоящий из значений, которые получены при
+# вычислении выражения block. Исходный массив при этом не изменяется.       array.map {|item| block }      #=> an_array
+
+# minmax Returns a two element array which contains the minimum and the maximum
+# value in the enumerable. The first form assumes all objects implement Comparable;
+# the second uses the block to return a <=> b.    minmax { |a, b| block } → [min, max]
+
+# reverse Возвращает новый массив, в котором элементы массива array стоят в обратном порядке.
+
+# join Возвращает строку, созданную путем преобразования каждого элемента
+# массива в строку, разделенных строкой     ["a", "b", "c" ].join("-")  #=> "a-b-c"
