7c756ddfe1099c8b8396d6a3eefaa693fb62018e
iiom <ioma766@gmail.com>
2019-02-16 18:25:09 +0300

    with not testing solution

--- my_friends_cheating.rb
+++ my_friends_cheating.rb
@@ -9,5 +9,17 @@ def removNb(n)
   results.sort!
 end
 
+# is to long time
+# def removNb(n)
+#   arr_numb = (1..n).to_a
+#   arr_res = []
+#   arr_numb.combination(2).to_a.select do |elem|
+#     if (arr_numb.arr_sum - elem.arr_sum) == elem.inject(:*)
+#       arr_res << elem << elem.reverse
+#     end
+#   end
+#   arr_res
+# end
+#
 p removNb(26)
 p removNb(100)
