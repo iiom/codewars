96949e61be9e843ede87acfd41e26b14f4dfcb70
iiom <ioma766@gmail.com>
2019-02-25 19:37:17 +0300

    add best solution from kata

--- weight_weight.rb
+++ weight_weight.rb
@@ -5,3 +5,9 @@ end
 
 puts "#{order_weight("103 123 4444 99 2000")} <==> 2000 103 123 4444 99"
 puts "#{order_weight("2000 10003 1234000 44444444 9999 11 11 22 123")} <==> 11 11 2000 10003 22 123 1234000 44444444 9999"
+
+
+# best from kata
+# def order_weight(string)
+#   string.split.sort_by { |n| [n.chars.map(&:to_i).reduce(:+), n] }.join(" ")
+# end
