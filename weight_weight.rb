b2915b6bd5a011bc1a4abee560fbfe65fc4e27eb
iiom <ioma766@gmail.com>
2019-02-25 19:36:56 +0300

    complete kata

--- weight_weight.rb
+++ weight_weight.rb
@@ -1,7 +1,7 @@
 def order_weight(strng)
-
+  strng = strng.split(" ").map{|e| [e, e.split("").map(&:to_i).sum]}
+  strng.sort_by { |elem| [elem.last, elem.first] }.map(&:first).join(' ')
 end
 
 puts "#{order_weight("103 123 4444 99 2000")} <==> 2000 103 123 4444 99"
 puts "#{order_weight("2000 10003 1234000 44444444 9999 11 11 22 123")} <==> 11 11 2000 10003 22 123 1234000 44444444 9999"
-
