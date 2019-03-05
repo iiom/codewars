0c623d532e52cc83be363aeaed8afd217e229872
iiom <ioma766@gmail.com>
2019-03-06 00:02:00 +0300

    abir

--- simple_number_sequence.rb
+++ simple_number_sequence.rb
@@ -1,5 +1,29 @@
 def missing(s)
+  s = s.each_char.to_a.map(&:to_i)
+  m = []
+  puts "original #{s}"
+  (1..6).to_a.select do |i|
+    s.first(i).size * 2 <= s.size ? nil : next
+    fr = s.first(i).join.to_i
+    (fr + (s.size / i)).to_s.split('').size > s.first(i).size ? (ls = s.last(i + 1).join.to_i) : (ls = s.last(i).join.to_i)
+    ss = (fr..ls).to_a
+    next if (ss.uniq != ss) || ss.empty? || ss.size > s.size || (i == 1 && ss.size < s.size) #|| (i == 2 && ss.size * 2 < s.size)
 
+    ee = []
+    ss.each do |i|
+      e = i.to_s.split('').size
+      ee << s.slice!(0, e)
+    end
+
+    ee.map!(&:join).delete_if{|i| i.empty?}.map(&:to_i)
+
+    puts "fr #{fr}"
+    puts "ls #{ls}"
+    p ss
+    p ee
+    puts "_________________________"
+  
+  end
 
 end
 
