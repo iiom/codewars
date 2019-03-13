12a88417caa9c31729476d1b11f374de7b91d594
iiom <ioma766@gmail.com>
2019-03-13 23:59:04 +0300

    add kata solution

--- string_incrementer.rb
+++ string_incrementer.rb
@@ -9,3 +9,9 @@ end
 # def increment_string(input)
 #   input.sub(/\d*$/) { |n| n.empty? ? 1 : n.succ }
 # end
+
+
+
+# def increment_string(input)
+#   input[/\d\z/] ? input.sub(/(\d+)\z/) { $1.next } : input + '1'
+# end
