67f09db92d685807e7d31decafb6c5ac737911ee
iiom <ioma766@gmail.com>
2019-03-13 23:57:45 +0300

    add best kata solution

--- string_incrementer.rb
+++ string_incrementer.rb
@@ -3,3 +3,9 @@ def increment_string(input)
   n = input.scan(/\d+\z/).empty? ? ['0'] : input.scan(/\d+\z/)
   s + (n.first.to_i + 1).to_s.rjust(n.first.size, '0')
 end
+
+
+
+# def increment_string(input)
+#   input.sub(/\d*$/) { |n| n.empty? ? 1 : n.succ }
+# end
